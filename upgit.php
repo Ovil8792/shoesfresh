7d7df7a4f49b061c1ea6387744b347b350d9990e
Duynh-WD19301 <duynhnhpp03128@gmail.com~>
2025-12-24 22:31:57 +0700

    up sp

--- upgit.php
+++ upgit.php
@@ -452,4 +452,64 @@ Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và th
 
 Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
 
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.</p><p>Theo cáo trạng, từ tháng 4/2023, Chu Mạnh Cường thuê các bị cáo Cao Đắc Nguyên, Nguyễn Văn Quý, Đỗ Văn Cường, Trần Quốc Tuấn, Nguyễn Văn Nam, Hoàng Thế Nam, Hà Bá Thuyên, Đỗ Tùng Lâm, Lê Quang Hoàn giúp Cường tổ chức cho vay lãi nặng.
+
+Cường lập "Nhóm mới Nổi" trên ứng dụng Messenger để trao đổi thông tin người vay, đi đòi tiền vay. Bị can này phân công nhiệm vụ cho từng thành viên trong nhóm, như chạy quảng cáo tìm nguồn khách, xác minh điều kiện cho vay, trực tiếp cho vay, thu lãi, ghi chép sổ sách, nhắn tin đòi tiền, tạt sơn khi người vay không trả tiền…
+
+Với những trường hợp người vay chậm trả, Cường chỉ đạo người trong nhóm cho vay thống nhất nhắn tin, điện thoại, tạt sơn vào nhà "con nợ" để đòi tiền. Các thành viên được Cường lo chỗ ở, chi phí ăn uống và cuối năm được chia 30%/doanh thu, trả lương 80-100 triệu đồng/năm.
+
+Cường đã chia các đối tượng cho vay thành hai nhóm, thuê căn hộ chung cư cho các đồng phạm hoạt động cho vay tại TPHCM và tỉnh Bình Dương (cũ).Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.</p><p>Theo cáo trạng, từ tháng 4/2023, Chu Mạnh Cường thuê các bị cáo Cao Đắc Nguyên, Nguyễn Văn Quý, Đỗ Văn Cường, Trần Quốc Tuấn, Nguyễn Văn Nam, Hoàng Thế Nam, Hà Bá Thuyên, Đỗ Tùng Lâm, Lê Quang Hoàn giúp Cường tổ chức cho vay lãi nặng.
+
+Cường lập "Nhóm mới Nổi" trên ứng dụng Messenger để trao đổi thông tin người vay, đi đòi tiền vay. Bị can này phân công nhiệm vụ cho từng thành viên trong nhóm, như chạy quảng cáo tìm nguồn khách, xác minh điều kiện cho vay, trực tiếp cho vay, thu lãi, ghi chép sổ sách, nhắn tin đòi tiền, tạt sơn khi người vay không trả tiền…
+
+Với những trường hợp người vay chậm trả, Cường chỉ đạo người trong nhóm cho vay thống nhất nhắn tin, điện thoại, tạt sơn vào nhà "con nợ" để đòi tiền. Các thành viên được Cường lo chỗ ở, chi phí ăn uống và cuối năm được chia 30%/doanh thu, trả lương 80-100 triệu đồng/năm.
+
+Cường đã chia các đối tượng cho vay thành hai nhóm, thuê căn hộ chung cư cho các đồng phạm hoạt động cho vay tại TPHCM và tỉnh Bình Dương (cũ).Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.Các bị can đồng phạm được Cường giao nhiệm vụ trực tiếp duyệt cho vay, hướng dẫn thủ tục để người vay, chuyển tiền cho người vay, hướng dẫn nhân viên nhắn tin, gọi điện thoại đòi tiền vay.
+
+Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
+
 Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.</p>
