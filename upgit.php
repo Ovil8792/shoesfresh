55803417e0ee4dd8b505f2fc5263626365d54d88
Duynh-WD19301 <duynhnhpp03128@gmail.com~>
2025-12-24 22:34:26 +0700

    up sp

--- upgit.php
+++ upgit.php
@@ -542,7 +542,7 @@ Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và th
 
 Các bị can đã cho 8 bị hại vay tiền với lãi suất 280-720%/năm, cao gấp hơn 5 lần lãi suất quy định trong Bộ luật dân sự.
 
-Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.</p><p>Theo cáo trạng, từ tháng 4/2023, Chu Mạnh Cường thuê các bị cáo Cao Đắc Nguyên, Nguyễn Văn Quý, Đỗ Văn Cường, Trần Quốc Tuấn, Nguyễn Văn Nam, Hoàng Thế Nam, Hà Bá Thuyên, Đỗ Tùng Lâm, Lê Quang Hoàn giúp Cường tổ chức cho vay lãi nặng.
+Cường đã bỏ ra số tiền khoảng 2,2 tỷ đồng để cho vay và thu lợi bất chính hơn 4,1 tỷ đồng.</p>ccc<p>Theo cáo trạng, từ tháng 4/2023, Chu Mạnh Cường thuê các bị cáo Cao Đắc Nguyên, Nguyễn Văn Quý, Đỗ Văn Cường, Trần Quốc Tuấn, Nguyễn Văn Nam, Hoàng Thế Nam, Hà Bá Thuyên, Đỗ Tùng Lâm, Lê Quang Hoàn giúp Cường tổ chức cho vay lãi nặng.
 
 Cường lập "Nhóm mới Nổi" trên ứng dụng Messenger để trao đổi thông tin người vay, đi đòi tiền vay. Bị can này phân công nhiệm vụ cho từng thành viên trong nhóm, như chạy quảng cáo tìm nguồn khách, xác minh điều kiện cho vay, trực tiếp cho vay, thu lãi, ghi chép sổ sách, nhắn tin đòi tiền, tạt sơn khi người vay không trả tiền…
 
